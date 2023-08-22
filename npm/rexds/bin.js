5955be87f0c3af786aa0042844c1d95ff659036e
Sami Fouad <sfouad@gmail.com>
2023-08-21 22:14:56 -0600

    windows tests ok!!!omg. re-adding macos targets

--- npm/rexds/bin.js
+++ npm/rexds/bin.js
@@ -13,14 +13,14 @@ let binaryPath;
 switch (os.platform()) {
     case 'darwin':
         binaryPath = os.arch() === 'x64'
-            ? `./node_modules/rexds-darwin-x64/${binaryName}`
-            : `./node_modules/rexds-darwin-arm64/${binaryName}`;
+            ? `./node_modules/@fromafrica/rexds-darwin-x64/${binaryName}`
+            : `./node_modules/@fromafrica/rexds-darwin-arm64/${binaryName}`;
         break;
     case 'win32':
-        binaryPath = `./node_modules/rexds-windows-x64/${binaryName}`;
+        binaryPath = `./node_modules/@fromafrica/rexds-windows-x64/${binaryName}`;
         break;
     case 'linux':
-        binaryPath = `./node_modules/rexds-linux-x64-musl/${binaryName}`;
+        binaryPath = `./node_modules/@fromafrica/rexds-linux-x64-musl/${binaryName}`;
     default:
         console.error(`Unsupported platform: ${os.platform()}`);
         process.exit(1);
