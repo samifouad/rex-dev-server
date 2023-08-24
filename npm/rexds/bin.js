9dbcc145a9fdb4ea6b7cf86a86f19bee1550408b
Sami Fouad <sfouad@gmail.com>
2023-08-23 22:34:13 -0600

    fixing path resolution for npm binary

--- npm/rexds/bin.js
+++ npm/rexds/bin.js
@@ -3,19 +3,21 @@ const { execSync } = require('child_process');
 const os = require('os');
 
 let binaryPath;
-let npmGlobal = execSync("npm root -g").toString().trim();
 
 switch (os.platform().toString().trim()) {
     case 'darwin':
+        let dariwinNpmGlobal = execSync("npm root -g").toString().trim();
         binaryPath = os.arch() === 'x64'
-            ? npmGlobal +'/rexds/node_modules/@fromafrica/rexds-darwin-x64/rexds'
-            : npmGlobal +'/rexds/node_modules/@fromafrica/rexds-darwin-arm64/rexds';
+            ? dariwinNpmGlobal +'/rexds/node_modules/@fromafrica/rexds-darwin-x64/rexds'
+            : dariwinNpmGlobal +'/rexds/node_modules/@fromafrica/rexds-darwin-arm64/rexds';
         break;
     case 'win32':
-        binaryPath = npmGlobal +'\\rexds\\node_modules\\@fromafrica\\rexds-windows-x64\\rexds.exe';
+        let winNpmGlobal = execSync("npm root -g").toString().trim();
+        binaryPath = winNpmGlobal +'\\rexds\\node_modules\\@fromafrica\\rexds-windows-x64\\rexds.exe';
         break;
     case 'linux':
-        binaryPath = npmGlobal +'/rexds/node_modules/@fromafrica/rexds-linux-x64-musl/rexds';
+        let linuxNpmGlobal = execSync("npm root -g").toString().trim().split('/').slice(0, -1).join('/');
+        binaryPath = linuxNpmGlobal +'/rexds/node_modules/@fromafrica/rexds-linux-x64-musl/rexds';
         break;
     default:
         console.error(`Unsupported platform: ${os.platform()}`);
